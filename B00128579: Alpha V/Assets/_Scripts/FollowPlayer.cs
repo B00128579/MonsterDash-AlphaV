ab25fb31cc99b1c4e972fa4c4a45bbe46dafa976
B00128579 <71762776+B00128579@users.noreply.github.com>
2020-12-01 08:56:46 +0000

    Alpha V

---
+++ B00128579: Alpha V/Assets/_Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Transform targetObject;
+    private Vector3 cameraOffset = new Vector3(-50, 10, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - targetObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = targetObject.transform.position + cameraOffset;
+        transform.position = newPosition;
+    }
+}
